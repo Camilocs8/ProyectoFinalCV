5863568f411cfef77efbe6aa98a4c56901c0cc96
Camilo <67339267+Camilocs8@users.noreply.github.com>
2023-01-21 16:46:19 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,24 +1,3 @@
- // function myFunction() {
- //  var x = document.getElementById("myDIV");
- //  if (x.style.display === "none") {
-  //   x.style.display = "block";
-  // } else {
- //     x.style.display = "none";
- //  }
- // }
-
-  //onclick="myFunction()"
-
-
-//function myFunction(){
-  //document.getElementById('contacto').style.display = "block";
-//}
-
-//function hideall(){
- //document.getElementById('contacto').style.display = "none";
-//}
-
-
 $(document).ready(function(){
   $("button").click(function(){
     $("form").toggle();
